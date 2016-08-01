14ca3267d8b050391eaf435e4fb48e301fd35d01
Karelgit <karel0522@gmail.com>
2016-08-01 18:28:36 +0800

    H5_ImageLoader done/fix regex bug_2016-8-1 18:28:26

--- src/main/java/utils/ReadFile.java
+++ src/main/java/utils/ReadFile.java
@@ -1,9 +1,11 @@
 package utils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,7 +14,6 @@ import java.util.regex.Pattern;
  */
 public class ReadFile {
     public static String readByLine(String filePath)    {
-        List<String> image_src_list = new ArrayList<String>();
         File file = new File(filePath);
         BufferedReader reader = null;
         String lineString = "";
@@ -40,35 +41,21 @@ public class ReadFile {
         }
         return lineString;
     }
-
-    /*public static void outputFile(List<String> list) {
-        try {
-            File file = new File(System.getProperty("user.dir")+"\\data\\imgSrc.txt");
-            FileWriter fileWriter = new FileWriter(file);
-            for (String s : list) {
-                fileWriter.write(s);
-                fileWriter.write("\n");
-            }
-            fileWriter.close(); // 关闭数据流
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }*/
     public static void main(String[] args) throws IOException {
         String pro_path = System.getProperty("user.dir");
-        String html_path = pro_path + "/data/1.html";
-        String download_path = pro_path + "/data/";
+        String html_path = pro_path + "\\data\\1.html";
+        String download_path = pro_path + "\\data\\2016商务大气邀请函\\";
         List<String> imgList = new ArrayList<String>();
         String src =  readByLine(html_path);
-        Pattern p = Pattern.compile("(http://)[^(http)]*?(png|gif|src)");
+        System.out.println(src);
+        Pattern p = Pattern.compile("(http://)\\S+?(png|gif|jpg|jpeg|src)");
         Matcher m = p.matcher(src);
         while (m.find())    {
             imgList.add(m.group());
         }
         for (String s : imgList) {
-            String final_fileName = s.replace("/","*");
-            new  Download().download(s,download_path,final_fileName);
+            new  Download().download(s,download_path,s.replace("/","_").replace(":","_"));
         }
+//        System.out.println("http://img1.maka.im/gallery/pehtople/30de0fa5-e9cd-4553-8bf3-41470f622350.jpg".matches("(http://)\\S+?(png|gif|jpg|jpeg|src)"));
     }
 }
